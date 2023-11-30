1d0e70718386d14e18a1b31ecc4ca4c42e5d4810
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-11-30 19:08:15 -0300

    Start JS & prepare HTML id various

---
+++ script.js
@@ -0,0 +1,5 @@
+// HTML elements
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('scoreBoard');
+const start = document.getElementById('start');
+const gameOver = document.getElementById('gameOver');
