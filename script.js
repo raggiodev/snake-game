9b9d75321a3abeda01a3302f4b43afc5f1433002
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-11-30 19:14:11 -0300

    add variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // HTML elements
 const board = document.getElementById('board');
 const scoreBoard = document.getElementById('scoreBoard');
-const start = document.getElementById('start');
+const startButton = document.getElementById('start');
 const gameOver = document.getElementById('gameOver');
+
+// game settings
+const boardSize = 10;
+const gameSpeed = 100;
+const directions = { arrowUp: 10, arrowDown: -10, arrowLeft: 1, arrowRight: -1 }
+const squareTypes = { snakeSquare: 1, foodSquare: 2, emptySquare: 0 };
+
+// game variables
+let snake;
+let score;
+let direction;
+let moveInterval;
+let boardSquares;
+let emptySquares;
